391956579c7646b853f4b344197b80337767c421
Federico Esparza <federicoesparza@outlook.com>
2017-05-07 17:51:46 -0500

    Fix scroll and lobby message send broken in recent commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,5 +16,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( game-board.js )
 Rails.application.config.assets.precompile += %w( game-ui.js )
-Rails.application.config.assets.precompile += %w( game-websocket.js )
+# Rails.application.config.assets.precompile += %w( game-websocket.js )
 # Rails.application.config.assets.precompile += %w( game-update.js )
