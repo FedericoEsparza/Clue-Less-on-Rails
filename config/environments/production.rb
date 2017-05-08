5619d43727f45aea4095de02fb9a8144be1fa60b
Federico Esparza <federicoesparza@outlook.com>
2017-05-07 20:16:07 -0500

    Change config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['https://clue-less-on-rails-staging.herokuapp.com',
-        'http://clue-less-on-rails-staging.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://clue-less-on-rails.herokuapp.com',
+        'http://clue-less-on-rails.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -91,6 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://clue-less-on-rails-staging.herokuapp.com' }
-  config.action_cable.url = "wss://clue-less-on-rails-staging.herokuapp.com/cable"
+  config.action_mailer.default_url_options = { host: 'http://clue-less-on-rails.herokuapp.com' }
+  config.action_cable.url = "wss://clue-less-on-rails.herokuapp.com/cable"
 end
